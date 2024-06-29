4e65ac044b736d74d24df9ba0f91cf49099089c4
code jstar <codejstar@gmail.com>
2024-06-29 10:30:13 +0530

    final commit

---
+++ src/components/home/Carousel.js
@@ -0,0 +1,37 @@
+/* eslint-disable */
+import React from "react";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+const imageProp = ["pizza", "burger", "milkshake"];
+
+const CarouselComponent = () => {
+  return (
+    <Carousel
+      autoPlay
+      navButtonsAlwaysVisible
+      infiniteLoop
+      showStatus={false}
+      emulateTouch
+      showThumbs={false}
+    >
+      {imageProp.map((image, index) => {
+        return (
+          <div
+            key={index}
+            style={{ maxHeight: "36rem" }}
+            className="object-center brightness-50"
+          >
+
+            <img
+              src={`https://source.unsplash.com/random/900x600/?pizza`}
+              alt="pizza"
+            />
+          </div>
+        );
+      })}
+    </Carousel>
+  )
+}
+
+export default CarouselComponent
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,16 @@
 import { ThemeProvider } from "next-themes";
+import "@/styles/globals.css";
 import Layout from "@/components/layouts/Layout";
+import { CartProvider } from "@/utils/ContextReducer";
 
 export default function App({ Component, pageProps }) {
   return (
      <ThemeProvider attribute="class">
+     <CartProvider>
         <Layout>
         <Component {...pageProps} />
         </Layout>
+      </CartProvider>
      </ThemeProvider>
   )
 }
